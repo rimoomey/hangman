3dca59d2411e7bd7ce11cc5e0f7e4f8e97b78305
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-17 11:22:49 -0500

    Add file reader and method to get specific line.

--- file_handler.rb
+++ file_handler.rb
@@ -2,7 +2,15 @@
 
 # handle input from dictionary and output to save
 module FileHandler
-  def create_word(file_name)
-    dictionary_data
+  def get_entire_contents(file_name)
+    dictionary_data = File.open(file_name, 'r')
+    dictionary_data.read
+  end
+
+  def get_specific_line(file_name, linenum)
+    File.open(file_name, 'r').each_with_index do |line,index|
+      return line if index + 1 == linenum
+    end
+    'That line is out of bounds.'
   end
 end
--- game.rb
+++ game.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: false
+
+require_relative './file_handler'
+
 class Game
+  include FileHandler
+
+  def choose_secret_word(data_file); end
 end
+
+hangman = Game.new
+puts hangman.get_specific_line('./google-10000-english.txt', 10)
