b090f7582417aa40f661358c5f83eae0f93988c4
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-17 11:42:15 -0500

    Create module for all text output.

---
+++ text.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: false
+
+# Module for the game's text responses.
+module Text
+  def instructions
+    puts '<Instructions go here>'
+  end
+
+  def prompt_guess
+    puts 'Guess a letter (A-Z):'
+  end
+end
