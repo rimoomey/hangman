b39cf0065cfff137a3d7cf972d91f08eb6fd0663
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-17 15:04:31 -0500

    Finish refactoring variables for saving.

--- game.rb
+++ game.rb
@@ -7,45 +7,45 @@ require_relative './text'
 class Game
   include FileHandler
   include Text
-  attr_reader :dictionary_file_path, :max_turns_per_game,
+  attr_reader :secret_word, :max_turns_per_game,
               :turns_left, :letters_guessed
 
-  def initialize(dictionary, max_turns_per_game)
-    @dictionary_file_path = dictionary
+  def initialize(max_turns_per_game)
     @max_turns_per_game = max_turns_per_game
     @turns_left = max_turns_per_game
     @letters_guessed = []
+    @secret_word = ' '
   end
 
-  def choose_secret_word
+  def choose_secret_word(file_name)
     line_number = rand(1..10_000)
-    word = get_specific_line(@dictionary_file_path, line_number)
+    word = get_specific_line(file_name, line_number)
 
-    word = choose_secret_word if word.length <= 5 || word.length >= 12
+    word = choose_secret_word(file_name) if word.length <= 5 || word.length >= 12
 
-    word
+    @secret_word = word
   end
 
-  def correct_guesses(secret_word)
-    corrects = Array.new(secret_word.length)
+  def correct_guesses
+    corrects = Array.new(@secret_word.length)
     corrects = corrects.map { '_' }
 
-    secret_word.split('').each_with_index do |value, index|
+    @secret_word.split('').each_with_index do |value, index|
       corrects[index] = value if @letters_guessed.include?(value)
     end
 
     corrects
   end
 
-  def win?(secret_word, correct_guesses)
-    secret_word.split('').each do |value|
+  def win?(correct_guesses)
+    @secret_word.split('').each do |value|
       return false unless correct_guesses.include?(value)
     end
     true
   end
 
-  def guessed_correctly?(secret_word, guess)
-    if secret_word.split('').include?(guess)
+  def guessed_correctly?(guess)
+    if @secret_word.split('').include?(guess)
       success_output
       0
     else
@@ -53,9 +53,9 @@ class Game
     end
   end
 
-  def new_game
+  def new_game(dictionary)
     print_instructions
-    secret_word = choose_secret_word
+    choose_secret_word(dictionary)
     win = false
 
     while @turns_left.positive? && !win
@@ -63,17 +63,17 @@ class Game
       current_guess = gets.chomp
       @letters_guessed.push current_guess
 
-      win = win?(secret_word, correct_guesses(secret_word))
+      win = win?(correct_guesses)
 
-      @turns_left -= guessed_correctly?(secret_word, current_guess)
-      show_correct_letters(correct_guesses(secret_word), @letters_guessed)
+      @turns_left -= guessed_correctly?(current_guess)
+      show_correct_letters(correct_guesses, @letters_guessed)
     end
-    reveal_word(secret_word, win)
+    reveal_word(@secret_word, win)
   end
 end
 
 dictionary_file = './google-10000-english.txt'
 max_turns = 15
 
-hangman = Game.new(dictionary_file, max_turns)
-hangman.new_game
+hangman = Game.new(max_turns)
+hangman.new_game(dictionary_file)
